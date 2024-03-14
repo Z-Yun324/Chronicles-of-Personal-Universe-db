a8e3096cc025061b82bc8b6026364bcd70250986
ZY324 <emma93324>
2024-03-14 12:22:56 +0800

    註冊密碼加密get

--- demo/src/main/java/com/example/demo/Service/EncrypAES.java
+++ demo/src/main/java/com/example/demo/Service/EncrypAES.java
@@ -11,7 +11,7 @@ public class EncrypAES {
    private KeyGenerator keygen;
    // SecretKey 負責保存對稱密鑰 
    private SecretKey deskey;
-   // Cipher 負責完成加密或解密工作 
+   // Cipher 加密對象負責完成加密或解密工作
    private Cipher c;
    //該字節數組負責保存加密的結果 
    private byte[] cipherByte;
@@ -21,34 +21,48 @@ public class EncrypAES {
        throws NoSuchAlgorithmException, NoSuchPaddingException
    {
        //Security.addProvider(new com.sun.crypto.provider.SunJCE());
-       // 實例化支持 AES 演算法的密鑰生成器(演算法名稱命名需按規定，否則拋出異常) 
+       // 實例化支持 AES 演算法的密鑰生成器(演算法名稱命名需按規定，否則拋出異常)
+       //algorithm加密類型
        keygen = KeyGenerator.getInstance("AES");
-       //生成密鑰 
+       //生成密鑰 加密規則?
        deskey = new SecretKeySpec(keyBytes, "AES");
-       //生成 Cipher 物件,指定其支持的 AES 演算法 
+       //生成 Cipher 物件,指定其支持的 AES 演算法 *transformation算法
        c = Cipher.getInstance("AES"); 
    } 
 
-   public byte[] Encrytor(String str) 
+   //加密
+   public byte[] Encrytor(String str)
        throws InvalidKeyException, IllegalBlockSizeException, 
               BadPaddingException 
    { 
-       // 根據密鑰，對 Cipher 物件進行初始化，ENCRYPT_MODE 表示加密模式 
+       // 根據密鑰，對 Cipher 物件進行初始化，ENCRYPT_MODE 表示加密模式  deskey規則
        c.init(Cipher.ENCRYPT_MODE, deskey); 
        byte[] src = str.getBytes(); 
-       // 加密，結果保存進 cipherByte 
-       cipherByte = c.doFinal(src); 
-       return cipherByte; 
+       // 加密方法，結果保存進 cipherByte 是亂碼
+       cipherByte = c.doFinal(src);
+       return cipherByte;
    } 
  
-
+//解密
    public byte[] Decryptor(byte[] buff) 
        throws InvalidKeyException, IllegalBlockSizeException,
            BadPaddingException
    { 
        // 根據密鑰，對 Cipher 物件進行初始化，DECRYPT_MODE 表示加密模式 
        c.init(Cipher.DECRYPT_MODE, deskey); 
-       cipherByte = c.doFinal(buff); 
-       return cipherByte; 
+       cipherByte = c.doFinal(buff);
+       return cipherByte;
    }
+/*
+    public static void main(String[] args) throws Exception {
+        //定義key
+        byte[] keyBytes = "0123456789abcdef".getBytes(); // 16字節的密鑰
+        EncrypAES de1 = new EncrypAES(keyBytes);
+        String msg ="1234567812345678";
+        byte[] encontent = de1.Encrytor(msg);
+        byte[] decontent = de1.Decryptor(encontent);
+        System.out.println("明文是:" + msg);
+        System.out.println("加密後:" + Base64.encodeBase64String(encontent));
+        System.out.println("解密後:" + new String(decontent));
+    }*/
 }
