0a6a544d430d52994977d93a06fc5a051eac05c3
Luin1223 <154805879+Luin1223@users.noreply.github.com>
2024-03-28 21:48:25 +0800

    Create Member.java

---
+++ demo/src/main/java/com/example/demo/pojo/Member.java
@@ -0,0 +1,23 @@
+package com.example.demo.pojo;
+
+public class Member {
+    String account;
+    String password;
+    public String getAccount() {
+        return account;
+    }
+
+    public void setAccount(String account) {
+        this.account = account;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+
+}
